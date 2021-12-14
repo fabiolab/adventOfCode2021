7de4cafd37766d7971a6a5296842c9a322656405
Fabrice Depaulis <fabrice.depaulis@orange.com>
2021-12-14 16:38:35 +0100

    day 14 star 2

--- day14.py
+++ day14.py
@@ -1,45 +1,42 @@
-from typing import List, Tuple
+from math import floor
+from typing import Dict
+
+FILEPATH = "data/data_14.txt"
 
 
-def fold_matrix_by_height(matrix: List[List[bool]], row: int):
-    return [[x1 or x2 for x1, x2 in zip(matrix[i], matrix[-1 - i])] for i in range(row)]
+def count_pairs(counter: Dict) -> Dict:
+    new_counter = counter.copy()
+    # All the existing pairs disapear as they are split while applying the rules
+    for pair in counter.keys():
+        new_counter[pair] = 0
+    # Increment the pair counter: from a given pair, two new pairs are generated
+    for pair, nb_pair in counter.items():
+        new_counter[f"{pair[0]}{PAIR_INSERTION[pair]}"] = new_counter.get(f"{pair[0]}{PAIR_INSERTION[pair]}",
+                                                                          0) + nb_pair
+        new_counter[f"{PAIR_INSERTION[pair]}{pair[1]}"] = new_counter.get(f"{PAIR_INSERTION[pair]}{pair[1]}",
+                                                                          0) + nb_pair
+    return new_counter
 
-
-def fold_matrix_by_width(matrix: List[List[bool]], col: int):
-    return [[y1 or y2 for y1, y2 in zip(line[0:col], reversed(line[col:]))] for line in matrix]
-
-
-def display_area(area: List[List[bool]]):
-    for y in area:
-        print("".join(['#' if x else '.' for x in y]))
-
-
-# COLS = 11
-# ROWS = 15
-COLS = 655 * 2 + 1
-ROWS = 447 * 2 + 1
 
 if __name__ == "__main__":
-    FILEPATH = "data/data_13.txt"
+    input_template = "HHKONSOSONSVOFCSCNBC"
+
+    # Init the "pair" counter with the initial sentence
+    pair_counter = {}
+    for a_pair in zip(input_template, input_template[1:]):
+        pair_counter["".join(a_pair)] = pair_counter.get("".join(a_pair), 0) + 1
 
     with open(FILEPATH, 'r') as fp:
-        DATA = [line.strip() for line in fp.readlines()]
+        PAIR_INSERTION = {line.strip().split(' -> ')[0]: line.strip().split(' -> ')[1] for line in fp.readlines()}
 
-    fold: List[List[bool]] = [[False] * COLS for i in range(ROWS)]
-    instructions: List[Tuple[str, int]] = []
-    for line in DATA:
-        if not line:
-            continue
-        if "fold" in line:
-            instructions.append((line.split('=')[0][-1], int(line.split('=')[1]),))
-        else:
-            fold[int(line.split(',')[1])][int(line.split(',')[0])] = True
+    for i in range(40):
+        pair_counter = count_pairs(pair_counter)
 
-    for direction, value in instructions:
-        match direction:
-            case 'y':
-                fold = fold_matrix_by_height(fold, value)
-            case 'x':
-                fold = fold_matrix_by_width(fold, value)
-        print(len([col for row in fold for col in row if col]))
-        display_area(fold)
+    # Count chars from pairs
+    char_counter = {}
+    for pair in pair_counter.keys():
+        char_counter[pair[0]] = char_counter.get(pair[0], 0) + pair_counter[pair]
+        char_counter[pair[1]] = char_counter.get(pair[1], 0) + pair_counter[pair]
+
+    print(floor(char_counter[max(char_counter, key=char_counter.get)] / 2) - floor(
+        char_counter[min(char_counter, key=char_counter.get)] / 2) - 1)
